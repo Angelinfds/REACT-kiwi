5650972ca3bd7ef112be105e05ff922d13c4c462
Angelin D Fernandez <adfds05@gmail.com>
2025-07-08 10:37:05 -0400

    implement todo list display and remove unused CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
+
 import './App.css'
 
 function App() {
- 
-
+  
+const todos = [
+    {id: 1, title: "review resources"},
+    {id: 2, title: "take notes"},
+    {id: 3, title: "code out app"},
+]
+  
   return (
-    <>
-      
-    </>
+  <div><h1>My todos</h1>
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+  
+  </div>
   )
 }
 
