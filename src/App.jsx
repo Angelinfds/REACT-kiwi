45912ae5739c8c61f0ccebe9be354cd6817357c0
Angelin D Fernandez <adfds05@gmail.com>
2025-07-25 10:53:53 -0400

    update todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
+import TodoList from './Todolist'
+import TodoForm from './Todoform'
 import './App.css'
 
 function App() {
- 
+  
 
+  
   return (
-    <>
-      
-    </>
+  <div><h1>My todos</h1>
+    <TodoForm />
+    <TodoList />
+  </div>
   )
 }
 
