fbb2d93120f2a3f617990d2307f454e193d14100
Angelin D Fernandez <adfds05@gmail.com>
2025-07-30 09:32:39 -0400

    add TodoListItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import TodoList from './Todolist'
 import TodoForm from './Todoform'
 import './App.css'
+import { useState } from 'react';
 
 function App() {
   
+const [newTodo, setTodo] = useState("Hello :)")
 
   
   return (
   <div><h1>My todos</h1>
     <TodoForm />
+    <p>{newTodo}</p>
     <TodoList />
   </div>
-  )
+  );
 }
 
 export default App
